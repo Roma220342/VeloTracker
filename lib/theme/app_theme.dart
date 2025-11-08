9a456dc5bd7117af16f9d5844064e5ddf9ade71b
roma <diachuk.20060@gmail.com>
2025-11-08 12:57:55 +0200

    Initial commit: Added project structure, theme, and splash screen

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF009F5A);
+const Color primaryContainerColor = Color(0xFFE0F3DC);
+const Color onPrimaryColor = Color(0xFFFFFFFF);
+const Color secondaryColor = Color(0xFFF9C400);
+
+const Color errorColor = Color(0xFFB3261E);
+const Color errorContainerColor = Color(0xFFFEF7F6);
+
+const Color surfaceColor = Color(0xFFFFFFFF);
+const Color onSurfaceColor = Color(0xFFFAFAFA);
+const Color onSurfaceActiveColor = Color(0xFFF9F5FF);
+
+const Color textPrimaryColor = Color(0xFF121212);
+const Color textSecondaryColor = Color(0xFF5E5E5E);
+const Color textTertiaryColor = Color(0xFF9E9E9E);
+
+final ThemeData lightTheme = ThemeData(
+  useMaterial3: true,
+  scaffoldBackgroundColor: surfaceColor,
+  fontFamily: null,
+
+  colorScheme: const ColorScheme.light(
+    primary: primaryColor,
+    primaryContainer: primaryContainerColor,
+    onPrimary: onPrimaryColor,
+    secondary: secondaryColor,
+    surface: surfaceColor,
+    onSurfaceVariant: onSurfaceActiveColor,
+    error: errorColor,
+    errorContainer: errorContainerColor,
+  ),
+
+  textTheme: const TextTheme(
+    // H1 (Bold • 32)
+    headlineLarge: TextStyle(
+      color: textPrimaryColor,
+      fontWeight: FontWeight.w700,
+      fontSize: 32,
+    ),
+
+    // H2 (Bold • 24)
+    headlineMedium: TextStyle(
+      color: textPrimaryColor,
+      fontWeight: FontWeight.w700,
+      fontSize: 24,
+    ),
+
+    // H2 (Medium • 24)
+    headlineSmall: TextStyle(
+      color: textPrimaryColor,
+      fontWeight: FontWeight.w500,
+      fontSize: 24,
+    ),
+
+    // Body Regular • 16
+    bodyLarge: TextStyle(
+      color: textPrimaryColor,
+      fontWeight: FontWeight.w400,
+      fontSize: 16,
+    ),
+
+    // Body Medium • 16 (твій запит)
+    bodyMedium: TextStyle(
+      color: textPrimaryColor,
+      fontWeight: FontWeight.w500,
+      fontSize: 16,
+    ),
+
+    // Body Bold • 16 — зручно дати через titleMedium
+    titleMedium: TextStyle(
+      color: textPrimaryColor,
+      fontWeight: FontWeight.w700,
+      fontSize: 16,
+    ),
+
+    // Caption Regular • 12
+    labelSmall: TextStyle(
+      color: textSecondaryColor,
+      fontWeight: FontWeight.w400,
+      fontSize: 12,
+    ),
+  ),
+
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: primaryColor,
+      foregroundColor: onPrimaryColor,
+      minimumSize: const Size(358, 56),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50.0),
+      ),
+      textStyle: ThemeData.light().textTheme.bodyMedium,
+      alignment: Alignment.center,
+    ),
+  ),
+
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      backgroundColor: secondaryColor,
+      foregroundColor: textPrimaryColor,
+      minimumSize: const Size(358, 56),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50.0),
+      ),
+      textStyle: ThemeData.light().textTheme.bodyMedium,
+      alignment: Alignment.center,
+    ),
+  ),
+
+  inputDecorationTheme: InputDecorationTheme(
+    filled: true,
+
+    fillColor: WidgetStateColor.resolveWith((states) {
+      if (states.contains(WidgetState.focused)) {
+        return onSurfaceActiveColor;
+      }
+      return onSurfaceColor;
+    }),
+
+    hintStyle: ThemeData.light().textTheme.bodyLarge?.copyWith(
+      color: textTertiaryColor,
+    ),
+
+    prefixIconColor: WidgetStateColor.resolveWith((states) {
+      if (states.contains(WidgetState.focused)) {
+        return textPrimaryColor;
+      }
+      if (states.contains(WidgetState.error)) {
+        return errorColor;
+      }
+      return textTertiaryColor;
+    }),
+
+    suffixIconColor: WidgetStateColor.resolveWith((states) {
+      if (states.contains(WidgetState.focused)) {
+        return textPrimaryColor;
+      }
+      return textTertiaryColor;
+    }),
+
+    contentPadding: const EdgeInsets.symmetric(
+      vertical: 16.0,
+      horizontal: 16.0
+    ),
+
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(15.0),
+      borderSide: BorderSide.none,
+    ), 
+  ),
+
+  appBarTheme: AppBarTheme(
+    backgroundColor: surfaceColor,
+    surfaceTintColor: Colors.transparent,
+    elevation: 0,
+    titleTextStyle: ThemeData.light().textTheme.headlineSmall,
+    iconTheme: const IconThemeData(
+      color: textPrimaryColor,
+      size: 24
+    ),
+  ),
+
+  bottomNavigationBarTheme: BottomNavigationBarThemeData (
+    backgroundColor: surfaceColor,
+    elevation: 0,
+
+    selectedItemColor: primaryColor,
+    unselectedItemColor: textSecondaryColor,
+
+    selectedLabelStyle: ThemeData.light().textTheme.labelSmall,
+    unselectedLabelStyle: ThemeData.light().textTheme.labelSmall,
+    
+  ),
+
+);
